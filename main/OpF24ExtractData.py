832f82c2ae7fbe52caca2d9bc8b1f66168fac0bb
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2025-07-07 10:30:00 -0500

    Update OpF24ExtractData.py

--- main/OpF24ExtractData.py
+++ main/OpF24ExtractData.py
@@ -3,4 +3,9 @@ import streamlit as st
 st.markdown("<style> div { text-align: center } </style>", unsafe_allow_html=True)
 st.subheader('EXTRACT DATA')
 
+
+with st.form(key='form1'):
+    uploaded_file = st.file_uploader("Choose a csv file", type="csv")
+    submit_button2 = st.form_submit_button(label='Aceptar')
+
 st.divider()
