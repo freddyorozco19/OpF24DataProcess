bcb41d525e9f24b12fecf6eb1e172f905fa5af02
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2025-07-08 05:29:50 -0500

    Update OpF24ExtractData.py

--- main/OpF24ExtractData.py
+++ main/OpF24ExtractData.py
@@ -1,4 +1,32 @@
 import streamlit as st
+import datetime
+import base64
+import pandas as pd
+from io import BytesIO
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as mplt
+import matplotlib.font_manager as font_manager
+import mplsoccer
+from mplsoccer import Pitch, VerticalPitch, FontManager
+import sklearn
+from sklearn.preprocessing import StandardScaler
+from scipy.spatial import ConvexHull
+from matplotlib.colors import LinearSegmentedColormap
+import matplotlib.patheffects as path_effects
+from scipy.ndimage import gaussian_filter
+import seaborn as sns
+from matplotlib import colors as mcolors
+import requests
+from PIL import Image
+from matplotlib.patches import Rectangle
+import math
+import altair as alt
+import json
+import re
+import sklearn
+from sklearn.preprocessing import StandardScaler
 
 st.markdown("<style> div { text-align: center } </style>", unsafe_allow_html=True)
 st.subheader('EXTRACT DATA')
