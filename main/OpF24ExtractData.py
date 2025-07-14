a45c032bb5243919e90eb7f3223ad6cf9ef70d2a
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2025-07-14 15:50:25 -0500

    Update OpF24ExtractData.py

--- main/OpF24ExtractData.py
+++ main/OpF24ExtractData.py
@@ -57,7 +57,6 @@ with st.form(key='form4'):
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file, sep=';')
 
-
 df = df.iloc[6:].reset_index(drop=True)
 df = df.drop(columns=['timestamp', 'date'])
 
