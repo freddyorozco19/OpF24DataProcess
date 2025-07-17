d1c65cbb2290e1e8600368659c6f24cf6c09f2ed
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2025-07-16 23:44:07 -0500

    Update OpF24DataProcess.py

--- OpF24DataProcess.py
+++ OpF24DataProcess.py
@@ -30,6 +30,8 @@ import requests
 from matplotlib.patches import Rectangle
 import math
 from PIL import Image
+
+
 ################################################################################################################################################################################################################################################################
 im = Image.open("Resources/Isotipo-FF046.ico")
 st.set_page_config(layout="wide", page_icon=im)
