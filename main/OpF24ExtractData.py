8f2f273b2229945b74cb993844e7b99f63ee00f0
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2025-08-21 11:24:06 -0500

    Update OpF24ExtractData.py

--- main/OpF24ExtractData.py
+++ main/OpF24ExtractData.py
@@ -480,7 +480,7 @@ DefBlocks_df = defWS_df[(defWS_df['type_id'] == 'Save') & (defWS_df['DefBlock'] 
 #DefBlocks_df = defWS_df[(defWS_df['DefBlock'] == True)].reset_index(drop=True)
 DefBlocks_Conteo = DefBlocks_df.groupby(['matchId', 'matchday', 'player_id', 'player_name', 'team_id']).size().reset_index(name='TotalDefBlocks')
 
-Tackles_df = defWS_df[(defWS_df['Event'] == 'Tackle')].reset_index(drop=True)
+Tackles_df = defWS_df[(defWS_df['type_id'] == 'Tackle')].reset_index(drop=True)
 Tackles_Conteo = Tackles_df.groupby(['matchId', 'matchday', 'player_id', 'player_name', 'team_id']).size().reset_index(name='TotalTackles')
 
 Resultado_Def = def_Conteo.merge(Clearance_Conteo, on=['matchId', 'matchday', 'player_id', 'player_name', 'team_id'], how='left')
