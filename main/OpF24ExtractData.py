6bf174307c534a685c3e690a4ee79b9a4445f4b0
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2025-08-22 07:02:06 -0500

    Update OpF24ExtractData.py

--- main/OpF24ExtractData.py
+++ main/OpF24ExtractData.py
@@ -277,7 +277,7 @@ with filteropt01:
         df = dfbk_filteropt_11
     else:
         df = df[df['NextPlayer'] == NextPlayerSel].reset_index(drop=True)
-df = df[['matchday', 'matchId', 'team_id', 'player_id', 'player_name', 'Event', 'type_id', 'outcome', 'LastEvent', 'NextEvent', 'NextPlayer', 'min', 'sec', 'x', 'y', 'x2', 'y2', 'qualifiers', 'qualifiers2', 'qualifiers3', 'Cross', 'CornerTaken', 'Throw-in', 'ChanceCreated', 'Assist', 'Handball', 'Overrun', 'AerialFoul', 'DuelosOfensivos', 'DuelosDefensivos', 'Blocked', 'DefBlock']]
+df = df[['matchday', 'matchId', 'team_id', 'player_id', 'player_name', 'Event', 'type_id', 'outcome', 'LastEvent', 'NextEvent', 'NextPlayer', 'min', 'sec', 'x', 'y', 'x2', 'y2', 'qualifiers', 'qualifiers2', 'qualifiers3', 'Cross', 'CornerTaken', 'Throw-in', 'ChanceCreated', 'Assist', 'KeyPass', 'Handball', 'Overrun', 'AerialFoul', 'DuelosOfensivos', 'DuelosDefensivos', 'Blocked', 'DefBlock']]
 
 ################################################################################################################################################################################################################################################################################################################################################
 
